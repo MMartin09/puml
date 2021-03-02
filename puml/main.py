48290ff0182c99fcf8745071bafa01fd7f5917b1
Moser Martin <m.moser@schreder-cms.com>
2021-03-02 14:31:09 +0100

    Add some project related files (black config, flake config, ...)

--- puml/main.py
+++ puml/main.py
@@ -13,28 +13,28 @@ def test(cls):
 def main():
     module_path: str = "../test_module/"
 
-    for _, name, is_pkg in pkgutil.walk_packages([module_path], f"test_module."):
-        print(name)
-        print("")
-
-        if not is_pkg:
-            module = importlib.import_module(name)
-
-            #for i in inspect(module):
-            #    print(module)
-
-            for key in dir(module):
-                type = getattr(module, key)
-
-                if inspect.isclass(type):
-                    print(f"Found class: {key}")
-
-                    print(test(type))
-
-                    m = inspect.getmembers(type)
-                    #print(m)
-
-        print("---------------------\n")
+    # for _, name, is_pkg in pkgutil.walk_packages([module_path], f"test_module."):
+    #     print(name)
+    #     print("")
+    #
+    #     if not is_pkg:
+    #         module = importlib.import_module(name)
+    #
+    #         #for i in inspect(module):
+    #         #    print(module)
+    #
+    #         for key in dir(module):
+    #             type = getattr(module, key)
+    #
+    #             if inspect.isclass(type):
+    #                 print(f"Found class: {key}")
+    #
+    #                 print(test(type)[0][1])
+    #
+    #                 m = inspect.getmembers(type)
+    #                 #print(m)
+    #
+    #     print("---------------------\n")
 
 
 if __name__ == '__main__':
